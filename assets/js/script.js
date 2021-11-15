04c5ca47e9c796c25ee2698c11b8a2643b7fdaab
Fabian <therealfabi@hotmail.com>
2021-11-14 23:46:21 -0300

    hola

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const MIN_NUM = 1;
+const MAX_NUM = 20;
+const BASE_NUM_FACT = 1;
+const ITERATION_NUM_FACT = 2;
+const numero = window.prompt(
+  "Ingrese N°, para obtener su tabla de multiplicacion y factorial"
+);
+const numero_parseado = parseInt(numero);
+if (
+  typeof numero_parseado === "NaN" ||
+  isNaN(numero_parseado) ||
+  numero_parseado < MIN_NUM ||
+  numero_parseado > MAX_NUM
+) {
+  window.alert(
+    "Porfavor, Ingrese N° y este debe ser mayor o igual a 1 y menor o igual a 20."
+  );
+  throw new Error(
+    "Porfavor, Ingrese N° y este debe ser mayor o igual a 1 y menor o igual a 20. Stop script!!"
+  );
+}
+const factorial = function (numero_factorial) {
+  let response = BASE_NUM_FACT;
+  for (let i = ITERATION_NUM_FACT; i <= numero_factorial; i++)
+    response = response * i;
+  return response;
+};
+const multiplicar = function (factor, coeficiente) {
+  return factor * coeficiente;
+};
+
+let mensaje = "";
+
+for (let indexMulti = 1; indexMulti <= numero; indexMulti++) {
+  mensaje += `${indexMulti} x ${numero} = ${multiplicar(indexMulti, numero)}\n`;
+}
+for (let indexFact = 1; indexFact <= numero; indexFact++) {
+  mensaje += `Factorial de ${indexFact} es  ${factorial(indexFact)}\n`;
+}
+
+window.console.log(mensaje);
+window.alert(mensaje);
